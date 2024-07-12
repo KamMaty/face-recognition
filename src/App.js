7fd83026343fe0db0d6b96c5e2e15608ef6c53ec
Kamil Matyasik <matyasik.k@wp.pl>
2024-07-13 00:46:24 +0200

    Added Clarify API for detecting faces and displaying box around - single face now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
-import ImagelinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        {/* TODO - Configurate background animation with tsparticles */}
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImagelinkForm />
-        {/* <FaceRecognition /> */}
-      </div>
-    );
-  }
+// configurate object for fetching
+
+const setupClarifaiRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the Account's Security section
+  const PAT = "26beae7f3fb0456589f393a140418ea7";
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = "mutu";
+  const APP_ID = "face-recognition";
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = "face-detection";
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Key " + PAT,
+    },
+    body: raw,
+  };
+
+  return requestOptions;
+};
+
+function App() {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [box, setBox] = useState({});
+
+  // Creating face box snippets
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  const displayFaceBox = (box) => {
+    setBox(box);
+  };
+
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    setImageUrl(input);
+    fetch(
+      "https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs",
+      setupClarifaiRequestOptions(imageUrl)
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        displayFaceBox(calculateFaceLocation(result));
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  return (
+    <div className="App">
+      {/* TODO - Configurate background animation with tsparticles */}
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm
+        onInputChange={onInputChange}
+        onButtonSubmit={onButtonSubmit}
+      />
+      <FaceRecognition box={box} imageUrl={imageUrl} />
+    </div>
+  );
 }
 
 export default App;
